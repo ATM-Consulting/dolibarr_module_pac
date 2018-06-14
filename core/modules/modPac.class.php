adc81624842ce97331018eb6eb393379f788c5ec
root <root@VMdebian>
2018-06-14 11:56:16 +0200

    DEL: spécifique Multigraph/Multi-Tec: désactivation page principale du module

--- core/modules/modPac.class.php
+++ core/modules/modPac.class.php
@@ -208,7 +208,7 @@ class modPac extends DolibarrModules
 									'titre'=>'PacLeftMenu',
 									'mainmenu'=>'commercial',
 									'leftmenu'=>'pac',
-									'url'=>'/pac/pac.php',
+									'url'=>'best_propales.php',
 									'langs'=>'pac@pac',	        // Lang file to use (without .lang) by module. File must be in langs/code_CODE/ directory.
 									'position'=>100,
 									'enabled'=>'$conf->pac->enabled',  // Define condition to show or hide menu entry. Use '$conf->pac->enabled' if entry must be visible if module is enabled. Use '$leftmenu==\'system\'' to show if leftmenu system is selected.
